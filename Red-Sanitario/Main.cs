19cd2db9deb2e469cffa59ae45cd5939b7aef045
Andres Rios <rianby64@gmail.com>
2017-04-16 18:12:14 +0300

    Commit inicial

---
+++ Red-Sanitario/Main.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+[TransactionAttribute(TransactionMode.Manual)]
+[RegenerationAttribute(RegenerationOption.Manual)]
+public class RedSanitario : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        //Get application and document objects
+        UIApplication uiApp = commandData.Application;
+        Document doc = uiApp.ActiveUIDocument.Document;
+
+        Transaction trans = new Transaction(doc);
+        trans.Start("Lab");
+
+        trans.Commit();
+        return Result.Succeeded;
+    }
+}
+
